b4dc65dcf8c583d39ef6b12669c0d2ca5a1b21bf
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-11-06 10:11:09 +0530

    Update Solution.c

--- Answer for 3.21/Solution.c
+++ Answer for 3.21/Solution.c
@@ -1,77 +1,35 @@
 #include <stdio.h>
-
-#include <unistd.h>
-
 #include <stdlib.h>
-
 #include <sys/wait.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<string.h>
+#include <unistd.h>
+#include <time.h>
 
-int main()
-{
-char c,num[10];
-int n,len,i;
-while(1)
- {
- printf("\nWhat is your number");
- gets(num);//Read input from console
- if(strcmp(num,"q")==0)// Quit if input is q
- exit(0);
- n=atoi(num);// String to number conversion
- if(n>2)// Wrong input checking
-  {
-        collatz(n);// Function calling
-  }
- else
-  {
-        printf("enter valid input...\n");
-  }
+int main(int argc, char *argv[]){
 
- }
-return 0;
-
-}
-
-int collatz(int i)// Called Function
- {
-
-  pid_t child_pid, wpid;
-
-  int status = 0;
-
-        if ((child_pid = fork()) == 0)// Child process creation
-
-   {
-
-    while(i != 1){
-
-        printf("%d\t", i);
-
-        if(i%2 == 0){// If n is even
-
-            i /= 2;
-
-           }
-
-        else{// if n is odd
-
-          i = 3*i + 1;
-
-         }
-
-       }
-
-   printf("1\n");
-   exit(1);
-
-   }
- while ((wpid = wait(&status)) > 0)
-
-   {
-
-   printf("Child process exited\n");
-
-   }
-
-return;
-
+    int n;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    if(n > 0){
+        int id = fork();
+        if(id == 0){
+            printf("Child process is printing = \t");
+            int i = n;
+            while(i != 1){
+                printf("%d\t",i);
+                if(i %2 == 0)
+                    i = i/2;
+                else
+                    i = 3*i + 1;
+            }
+        }
+        else{
+            printf("Parent process waiting for child to finish...\n");
+            wait(NULL);
+        }
+        if(id == 0)
+            printf("1\n");
+    }
 }
