92b7e9220d7f14feab6fb4ab44b213b7fc6a2ef1
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-11-06 10:11:41 +0530

    Update Solution.c

--- Answer for 3.27/Solution.c
+++ Answer for 3.27/Solution.c
@@ -1,60 +1,59 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<fcntl.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#define size 50
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
 
-int main(int arvuments, char* arv[]) 
-{
-    int file[2];
-    pid_t childid;
-    char readBuff[size];
-    pipe(file);
-    if( arvuments != 3 ) 
-    {
-        printf("ERROR: Need exactly 2 parameters.\n");
-        exit(1);
+int main(int argc, char *argv[]){
+
+    FILE* input;
+    FILE* copy;
+    char str[1000];
+    input = fopen("input.txt", "a+");
+    copy = fopen("copy.txt", "w");
+ 
+    if (input == NULL) {
+        printf("file can't be opened \n");
     }
-    int fileOpen = open(arv[1], 0);
-    int TargetFile = open(arv[2], O_RDWR|O_CREAT|O_APPEND, 0666);
-    if (fileOpen == -1 || TargetFile == -1) 
-    {
-        printf("File can not be opend\n");
-        exit(1);
+ 
+ 
+    fgets(str, 1000, input);
+
+
+
+    int fd[2];
+    if(pipe(fd) == -1)
+        return 1;
+    int pid = fork();
+
+    if(pid == 0){
+        
+        close(fd[0]);
+        str[strlen(str) - 1] = '\0';
+
+        int n = strlen(str) + 1;
+        write(fd[1], &n, sizeof(int));
+        write(fd[1], str, sizeof(char)*n);
+
+        close(fd[1]);
     }
-    childid = fork();
-    if (childid == 0) 
-    {
-        close(file[1]);
-	while (read(file[0], readBuff, sizeof(readBuff)) > 0) 
-	{
-		write(TargetFile, readBuff, strlen(readBuff) - 1);
-        }
-        close(file[0]);
-        close(TargetFile);
-    }
-    else 
-    {
-        close(file[0]);
-	while (read(fileOpen, readBuff, sizeof(readBuff)) > 0) 
-	{
-            write(file[1], readBuff, sizeof(readBuff));
-            memset(readBuff, 0, size);
-        }
-        close(file[1]);
-        close(fileOpen);
+    else{
+
+        close(fd[1]);
+        char str[1000];
+        int n;
+
+        read(fd[0], &n, sizeof(int));
+        read(fd[0], str, sizeof(char)*n);
+
+        fputs(str, copy);
+        close(fd[0]);
         wait(NULL);
     }
-}
 
-/*
-// enter the following commands in terminal to run the code
-# gcc filecopy.c -o cp
-# echo "enter any data here." > input.txt
-# ./cp input.txt copy.txt
-# cat copy.txt 
-*/
+    fclose(input);
+    fclose(copy);
+}
