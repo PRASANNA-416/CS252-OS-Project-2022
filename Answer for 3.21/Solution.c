00da7706466744aadf04d31aee7fef6976f991fa
Prasanna <76594416+PRASANNA-416@users.noreply.github.com>
2022-11-07 09:55:18 +0530

    Update Solution.c

--- Answer for 3.21/Solution.c
+++ Answer for 3.21/Solution.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/wait.h>
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<string.h>
+#include <string.h>
 #include <unistd.h>
-#include <time.h>
 
 int main(int argc, char *argv[]){
 
